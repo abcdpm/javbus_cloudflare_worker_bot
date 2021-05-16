34828b80b8c46c44283a9a79b311eca57e8961d3
lIlIAIex <33725411+abcdpm@users.noreply.github.com>
2021-05-16 20:21:50 +0800

    Update teleWorker.js

--- src/handlers/teleWorker.js
+++ src/handlers/teleWorker.js
@@ -69,8 +69,8 @@ export default async request => {
             if (resultList[i].has_subtitle) {
               messageText += " [字幕]"
             }
-            messageText += " " + resultList[i].size
             messageText += " " + resultList[i].releaseDate
+            messageText += '\n大小: ' + resultList[i].size
             messageText += '\n链接: <code>' + resultList[i].magnet + '</code>'
             if ((i + 1) > max) {
               break;
